b50f2dc47fcf5ad0e22f969518321c275ade38a4
ray36512 <40697774+ray36512@users.noreply.github.com>
2024-08-30 10:53:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 
-__version_info__ = (5, 0, 2)
+__version_info__ = (1, 0, 0)
 __version__ = '.'.join([str(v) for v in __version_info__])
 
 setup(
     name="Unum",
     version=__version__,
     description="Units in Python",
-    author="Chris MacLeod, Pierre Denis, Leszek Trzemecki",
-    author_email="leszek.trzemecki@gmail.com",
-    url="https://github.com/trzemecki/Unum",
+    author="Carlos Raymundo",
+    author_email="carlos.raymundo.luyo@gmail.com",
+    url="https://github.com/ray36512/pmd_unit/Unum",
     license="LGPL",
     install_requires=[
         'six'
